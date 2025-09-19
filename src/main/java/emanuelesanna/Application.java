c11fc9a8dddc28ea4fa2ed7864a0c26e476a035e
EmanSan9 <sanna950@gmail.com>
2025-09-19 12:21:30 +0200

    V4
    Fix nel while iniziale ora ritorna all'inizio

--- src/main/java/emanuelesanna/Application.java
+++ src/main/java/emanuelesanna/Application.java
@@ -50,43 +50,43 @@ public class Application {
             System.out.println("Premi 6 per aggiornare un elemento esistente dato l'ID");
             System.out.println("Premi 7 per stampare le statistiche della collezione");
 
+            int scelta1 = -1;
+
             try {
-                int scelta1 = Integer.parseInt(scan1.nextLine());
+                scelta1 = Integer.parseInt(scan1.nextLine());
             } catch (NumberFormatException e) {
-                System.err.println("Scelta non valida, digita un numero da 1 a 7!");
+                System.err.println("Scelta non valida, digita un numero da 0 a 7!");
             }
 
-//        switch (scelta1) {
-            case 0:
-
-                continua = false;
-                break;
-            System.out.println("Programma terminato");
-
-//            case 1:
+            switch (scelta1) {
+                case 0:
+                    continua = false;
+                    System.out.println("Programma terminato");
+                    break;
+                case 1:
 
 //            case 2:
-            System.out.println("Digita un ID");
-            try {
-                int scelta2 = Integer.parseInt(scan1.nextLine());
-                Gioco gioco1 = coll1.cercaPerId(scelta2);
+                    System.out.println("Digita un ID");
+                    try {
+                        int scelta2 = Integer.parseInt(scan1.nextLine());
+                        Gioco gioco1 = coll1.cercaPerId(scelta2);
 
-                if (gioco1 != null) {
-                    System.out.println("Gioco trovato:");
-                    System.out.println(gioco1);
-                } else {
-                    System.err.println("Nessun gioco trovato con ID " + scelta2);
-                }
-            } catch (NumberFormatException e) {
-                System.err.println("Errore: devi inserire un numero intero valido!");
-            }
-            break;
+                        if (gioco1 != null) {
+                            System.out.println("Gioco trovato:");
+                            System.out.println(gioco1);
+                        } else {
+                            System.err.println("Nessun gioco trovato con ID " + scelta2);
+                        }
+                    } catch (NumberFormatException e) {
+                        System.err.println("Errore: devi inserire un numero intero valido!");
+                    }
+                    break;
 //            case 3:
 //            case 4:
 //            case 5:
 //            case 6:
 //            case 7:
-//        }
+            }
         }
     }
 
